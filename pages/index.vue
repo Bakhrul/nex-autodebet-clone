8b571f336991b61f559e0479ed2531899a9e062d
Bakhrul <bakhrulbila@gmail.com>
2024-02-28 14:16:56 +0700

    push

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
-   Welcome
+   <main-app></main-app>
   </div>
 </template>
 
 <script>
+import MainApp from '~/components/MainApp.vue';
  export default {
-  
+  components: {
+      
+      'main-app': MainApp
+    },
  }
 </script>
